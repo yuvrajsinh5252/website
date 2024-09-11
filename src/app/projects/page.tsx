1a9f1c3861e4694f0d8fb97f58badd2fe1efcdd8
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-09-11 18:53:48 +0530

    feat: added dotfiles project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,3 @@
-import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { PROJECTS } from "@/data/projectData";
 import { SquareArrowOutUpRight } from "lucide-react";
 import Link from "next/link";
@@ -6,17 +5,20 @@ import { SiGithub } from "react-icons/si";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <div className="flex items-center justify-center h-screen">
-        <div className="flex items-center justify-center flex-wrap">
-          {PROJECTS.map((project) => {
+    // <MaxWidthWrapper>
+    <div className="flex flex-col items-center h-screen no-scrollbar overflow-scroll">
+      <div className="flex flex-col max-sm:items-center max-w-screen-lg gap-10 mt-44">
+        <h1 className="text-5xl font-bold pl-2">Projects</h1>
+        <div className="flex items-center max-sm:flex-col justify-center flex-wrap h-full">
+          {PROJECTS.map((project, index) => {
             return (
-              <div className="flex justify-center p-2 gap-2 group">
-                <div
-                  key={project.title}
-                  className="flex w-[30rem] rounded-lg justify-between flex-col gap-4 p-4 bg-gray-500/10"
-                >
+              <div
+                key={index}
+                className="flex max-sm:w-full w-1/2 justify-center p-2 gap-2 group"
+              >
+                <div className="flex rounded-lg justify-between flex-col gap-4 p-4 bg-gray-500/10">
                   <div className="flex flex-col justify-center gap-4">
+
                     <div className="flex justify-between items-center">
                       <div className="flex gap-4 items-center">
                         <h1 className="text-2xl font-bold">
@@ -46,20 +48,22 @@ export default function Home() {
                     <p className="text-[14px] text-gray-400 leading-2 overflow-ellipsis overflow-hidden text-justify">{project.description}</p>
                   </div>
                   <div className="flex mt-5 flex-wrap gap-2">
-                    {project.tag.map((t) => {
+                    {project.tag.map((t, ind) => {
                       return (
-                        <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs leading-5 text-teal-300 ">
+                        <div key={ind} className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs leading-5 text-teal-300 ">
                           {t}
                         </div>
                       );
                     })}
                   </div>
                 </div>
+
               </div>
             );
           })}
         </div>
       </div>
-    </MaxWidthWrapper>
+    </div>
+    // </MaxWidthWrapper >
   );
 }
