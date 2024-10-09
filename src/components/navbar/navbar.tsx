5b5fe8c20d7b8e4b97e4fe27d92d2fcf04db79a3
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-10-10 00:57:59 +0530

    bruh

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -44,11 +44,10 @@ export default function Navbar() {
           animate={{ opacity: 1 }}
           className="flex left-1/2 -translate-x-1/2 fixed m-auto items-center justify-between w-fit h-9 rounded-2xl bg-gray-600/20 duration-300 ease-linear outline-none border-2 border-gray-300/20 border-opacity-20 backdrop-filter backdrop-blur-md gap-2 py-7 px-3 dark:text-white text-black transition-all"
         >
-          <motion.div
+          <div
             id="active-bg"
             className="dark:bg-gray-300/20 -mx-5 bg-opacity-10 h-9 rounded-2xl absolute bg transition-all duration-300 ease-linear flex items-center justify-center bg-black"
-            layoutId="active-bg"
-          ></motion.div>
+          ></div>
           <NavLink setActiveNav={setActiveNav} activeNav={activeNav} text="home" href="/" />
           <NavLink setActiveNav={setActiveNav} activeNav={activeNav} text="projects" href="/projects" />
           <NavLink setActiveNav={setActiveNav} activeNav={activeNav} text="about" href="/about" />
