fa8c04ad58e7454fb6709bf8c4061ef2bf403b51
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2024-12-11 23:45:30 +0530

    navbar fix

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -15,8 +15,11 @@ export default function Navbar() {
       const activeBg = document.getElementById("active-bg");
       const activeLink = document.querySelector(`[href="${activeNav}"]`);
       const firstLink = document.querySelector(`[href="/"]`);
+      const navbar = document.getElementsByClassName(
+        "navbar"
+      )[0] as HTMLElement;
 
-      if (activeLink && activeBg && firstLink) {
+      if (activeLink && activeBg && firstLink && navbar) {
         const firstLinkRects = firstLink.getBoundingClientRect();
         const activeLinkRects = activeLink.getBoundingClientRect();
         const top = activeLinkRects.top - firstLinkRects.top;
@@ -25,8 +28,10 @@ export default function Navbar() {
         activeBg.style.width = `${activeLink.clientWidth + 30}px`;
         activeBg.style.height = `${activeLink.clientHeight}px`;
 
-        activeBg.style.top = `${top + (activeNav === "/" ? 7.5 : 11)}px`;
-        activeBg.style.left = `${left + (activeNav === "/" ? 28 : 15)}px`;
+        activeBg.style.top = `${top + (activeNav === "/" ? 0 : 4)}px`;
+        activeBg.style.left = `${left + (activeNav === "/" ? 20 : 16)}px`;
+
+        navbar.style.paddingLeft = `${activeNav === "/" ? 0 : 10}px`;
       } else if (activeBg) {
         activeBg.style.top = `0px`;
         activeBg.style.left = `0px`;
@@ -42,11 +47,11 @@ export default function Navbar() {
         <motion.nav
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          className="flex left-1/2 -translate-x-1/2 fixed m-auto items-center justify-between w-fit h-9 rounded-2xl bg-gray-600/20 duration-300 ease-linear outline-none border-2 border-gray-300/20 border-opacity-20 backdrop-filter backdrop-blur-md gap-2 py-7 px-3 dark:text-white text-black transition-all"
+          className="flex left-1/2 -translate-x-1/2 fixed m-auto items-center justify-between w-fit h-9 rounded-2xl dark:bg-gray-600/20 duration-300 ease-linear outline-none border-2 dark:border-gray-300/20 border-gray-800 border-opacity-20 backdrop-filter backdrop-blur-md gap-2 py-5 dark:text-white text-black transition-all navbar"
         >
           <div
             id="active-bg"
-            className="dark:bg-gray-300/20 -mx-5 bg-opacity-10 h-9 rounded-2xl absolute bg transition-all duration-300 ease-linear flex items-center justify-center bg-black"
+            className="dark:bg-gray-300/20 -mx-5 bg-opacity-10 h-9 rounded-xl absolute bg transition-all duration-300 ease-linear flex items-center justify-center bg-black"
           ></div>
           <NavLink
             setActiveNav={setActiveNav}
